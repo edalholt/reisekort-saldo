896ed79057b4c1166e859fc8acefb85383e9fab0
Eivind Dalholt <eividalh@stud.ntnu.no>
2024-12-23 00:06:56 +0100

    Adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs20'
+			runtime: 'nodejs20.x'
 		  })
 	}
 };
